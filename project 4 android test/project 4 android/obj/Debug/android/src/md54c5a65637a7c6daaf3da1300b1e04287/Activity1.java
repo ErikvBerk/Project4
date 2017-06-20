2d33b9819bf010cf648f322e476985817926d360
sibrenvanek <0924655@hr.nl>
2017-06-20 15:35:29 +0200

    added android project test

---
+++ project 4 android test/project 4 android/obj/Debug/android/src/md54c5a65637a7c6daaf3da1300b1e04287/Activity1.java
@@ -0,0 +1,47 @@
+package md54c5a65637a7c6daaf3da1300b1e04287;
+
+
+public class Activity1
+	extends md59e336b20c5f59a4196ec0611a339f132.AndroidGameActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("project_4_android.Activity1, project 4 android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", Activity1.class, __md_methods);
+	}
+
+
+	public Activity1 () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == Activity1.class)
+			mono.android.TypeManager.Activate ("project_4_android.Activity1, project 4 android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
