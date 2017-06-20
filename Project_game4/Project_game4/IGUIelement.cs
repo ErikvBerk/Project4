5820bed3924820b5b6526e7b87dfbda6be473372
ErikvBerk <erik.van.berk@gmail.com>
2017-06-20 11:36:58 +0200

    IGUIelement + start menu

---
+++ Project_game4/Project_game4/IGUIelement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Project_game4
+{
+    interface IGUIelement
+    {
+        void Draw(SpriteBatch spritebatch);
+        void Update(float dt);
+    }
+}
