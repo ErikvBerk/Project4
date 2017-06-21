4a12e9a7a8ac246489eecba68e3e6f9b5c240d7e
JVerhaar <0907856@hr.nl>
2017-06-21 15:27:00 +0200

    Buttons
    Zijn geinstantiate en design is aangepast.
    Achtergrond in Game1.cs ook.

--- Project_game4/Project_game4/Menu.cs
+++ Project_game4/Project_game4/Menu.cs
@@ -10,7 +10,7 @@ using Project_game4;
 
 namespace Project_game4
 {
-    class Menu :IGUIelement
+    class Menu : IGUIelement
     {
         GraphicsDeviceManager graphics;
         project_4_algemeen.button Play;
@@ -21,31 +21,41 @@ namespace Project_game4
         SpriteFont Font;
         Texture2D rect;
 
-        public Menu (GraphicsDeviceManager graphics,Texture2D rect,SpriteFont Font)
-            {
+        public Menu(GraphicsDeviceManager graphics, Texture2D rect, SpriteFont Font)
+        {
             this.graphics = graphics;
             this.Font = Font;
 
             //oud Play = new Button("Play", rect, 100, 200, this.Font,200,30,Color.Red);
-            Play = new project_4_algemeen.button(100, 100, 200, 30,"Play",Font, Color.Red, Color.Blue, () => Hi(),graphics);
+            Play = new project_4_algemeen.button(770, 400, 750, 200, "Play", Font, Color.DarkKhaki, Color.OliveDrab, () => Hi(), graphics);
+            Highscore = new project_4_algemeen.button(770, 550, 750, 200, "Highscores!", Font, Color.DarkKhaki, Color.OliveDrab, () => Hi(), graphics);
+            Instructions = new project_4_algemeen.button(770, 700, 750, 200, "Instructions!", Font, Color.DarkKhaki, Color.OliveDrab, () => Hi(), graphics);
+            Exit = new project_4_algemeen.button(770, 850, 750, 200, "Exit!", Font, Color.Maroon, Color.Red, () => Hi(), graphics);
 
         }
         public void Update()
         {
             Play.update();
-            
+            Highscore.update();
+            Instructions.update();
+            Exit.update();
+
         }
         public void Draw(SpriteBatch spritebatch)
         {
-            
+
             Play.draw(spritebatch);
-                //
-                //rect = new Texture2D(graphics.GraphicsDevice, 200, 30);
+            Highscore.draw(spritebatch);
+            Instructions.draw(spritebatch);
+            Exit.draw(spritebatch);
+            //
+            //rect = new Texture2D(graphics.GraphicsDevice, 200, 30);
 
         }
         public void Hi()
         {
 
         }
+
     }
 }
