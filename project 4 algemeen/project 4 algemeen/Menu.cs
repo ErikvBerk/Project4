e34360f3ef7e54a95c217e393d9e956a25cb186c
ErikvBerk <erik.van.berk@gmail.com>
2017-06-26 12:43:44 +0200

    100% working buttons

--- project 4 algemeen/project 4 algemeen/Menu.cs
+++ project 4 algemeen/project 4 algemeen/Menu.cs
@@ -12,43 +12,42 @@ namespace project_4_algemeen
     public interface game
     {
         gameElement current { get; set; }
+        
     }
     public interface gameElement
     {
         void update(game game1);
         void draw(SpriteBatch spritebatch);
-        List<button> buttons { get; }
+        //List<button> buttons { get; }
         List<textbox> textboxes { get; }
+        
     }
     public class Menu : gameElement
     {
-        GraphicsDeviceManager graphics;
-        public List<button> Buttons = new List<button>();
-        public List<textbox> Textboxes = new List<textbox>();
-        SpriteFont Font;
-        double screen_height;
-        double screen_width;
-        float relativeSize;
-        Action<game> exit;
+        public GraphicsDeviceManager graphics;
+        List<button> Buttons = new List<button>();
+        List<textbox> Textboxes = new List<textbox>();
+        public SpriteFont Font;
+        public double screen_height, screen_width;
+        public float relativeSize;
+        public Action<game> exit;
+        public game game1;
         
-        public Menu()
-        {
-
-        }
-        public Menu(GraphicsDeviceManager graphics, SpriteFont Font, double screen_height, double screen_width, float relativeSize, Action<game> exit)
+        
+        public Menu(double screen_width, double screen_height, float relativeSize, SpriteFont Font, GraphicsDeviceManager graphics,game game1)
         {
             this.graphics = graphics;
             this.Font = Font;
             this.screen_height = screen_height;
             this.screen_width = screen_width;
-            this.relativeSize = relativeSize;
-            this.exit = exit;
+            this.relativeSize = relativeSize;            
+            this.game1 = game1;
 
-            this.Textboxes.Add(new textbox(10, 10, 500, 200, Font, graphics));
-            Buttons.Add(new button((int)(screen_width * 0.8), (int)(this.screen_height * 0.18), (int)(screen_width * 0.4), (int)(screen_width * 0.1), "Play!", Font, relativeSize, Color.Khaki, Color.OliveDrab, (game1) => Hi1(game1), graphics));
-            Buttons.Add(new button((int)(screen_width * 0.8), (int)(this.screen_height * 0.26), (int)(screen_width * 0.4), (int)(screen_width * 0.1), "Highscores!", Font, relativeSize, Color.Khaki, Color.OliveDrab, (game1) => Hi2(game1), graphics));
-            Buttons.Add(new button((int)(screen_width * 0.8), (int)(this.screen_height * 0.34), (int)(screen_width * 0.4), (int)(screen_width * 0.1), "Instructions!", Font, relativeSize, Color.Khaki, Color.OliveDrab, (game1) => toInstructions(game1), graphics));
-            Buttons.Add(new button((int)(screen_width * 0.8), (int)(this.screen_height * 0.42), (int)(screen_width * 0.4), (int)(screen_width * 0.1), "Exit!", Font, relativeSize, Color.Maroon, Color.Red, (game1) => exit(game1), graphics));
+            this.Textboxes.Add(new textbox(10, 10, 500, 200, this.Font, graphics));
+            Buttons.Add(new button((int)(screen_width * 0.8), (int)(this.screen_height * 0.18), (int)(screen_width * 0.4), (int)(screen_width * 0.1), "Play!", Font, relativeSize, Color.Khaki, Color.OliveDrab, new Play("Play",this.screen_width,this.screen_height,this.relativeSize,this.Font,this.graphics,this.game1), this.graphics));
+            Buttons.Add(new button((int)(screen_width * 0.8), (int)(this.screen_height * 0.26), (int)(screen_width * 0.4), (int)(screen_width * 0.1), "Highscores!_instructions", Font, relativeSize, Color.Khaki, Color.OliveDrab, new Instructions("Instructions",this.screen_width,this.screen_height,this.relativeSize,this.Font,this.graphics,this.game1),this.graphics));
+            Buttons.Add(new button((int)(screen_width * 0.8), (int)(this.screen_height * 0.34), (int)(screen_width * 0.4), (int)(screen_width * 0.1), "Instructions!", Font, relativeSize, Color.Khaki, Color.OliveDrab, new Instructions("Instructions", this.screen_width, this.screen_height, this.relativeSize, this.Font, this.graphics, this.game1), graphics));
+            Buttons.Add(new button((int)(screen_width * 0.8), (int)(this.screen_height * 0.42), (int)(screen_width * 0.4), (int)(screen_width * 0.1), "Exit!", Font, relativeSize, Color.Maroon, Color.Red, new Instructions("Instructions", this.screen_width, this.screen_height, this.relativeSize, this.Font, this.graphics, this.game1), graphics));
         }
         public void update(game game1)
         {
@@ -72,13 +71,7 @@ namespace project_4_algemeen
                 t.draw(spritebatch);
             }
         }
-        public List<button> buttons
-        {
-            get
-            {
-                return this.Buttons;
-            }
-        }
+       
         public List<textbox> textboxes
         {
             get
@@ -86,21 +79,13 @@ namespace project_4_algemeen
                 return Textboxes;
             }
         }
-        private void toInstructions(game game1)
-        {
-            game1.current = new Instructies(screen_width, screen_height, Font, relativeSize, exit, graphics, "Menu");
-        }
-        public void Hi1(game game1)
-        {
-            
-        }
-        public void Hi2(game game1)
-        {
-            
-        }
-        public void Hi3(game game1)
-        {
-            
-        }
+
+        //List<button> gameElement.buttons
+        //{
+        //    get
+        //    {
+        //        return Buttons;
+        //    }
+        //}
     }
 }
