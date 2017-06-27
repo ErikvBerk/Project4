18d5fb8e37bfacab8c22298f1a3db38e94f73575
HogeschoolNiels <0925927@hr.nl>
2017-06-27 17:09:30 +0200

    Niels en Erik files, player en movement

---
+++ Project_game4/Project_game4/Enemy.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using project_4_algemeen;
+
+namespace project_4_algemeen
+{
+    class Enemy :gameElement
+    {
+        //will need position of player
+        int ID;
+        int HP, DMG;
+        int X, Y;
+        double screen_width, screen_height;
+        string name;
+        List<Texture2D> All_images = new List<Texture2D>();
+        List<button> Buttons = new List<button>();
+        List<textbox> Textboxes = new List<textbox>();
+
+        public Enemy(int ID,int X,int Y,double screen_width,double screen_height,List<Texture2D>All_images)
+        {
+            this.ID = ID;
+            this.X = X;
+            this.Y = Y;
+            this.All_images = All_images;
+            this.screen_height = screen_height;
+            this.screen_width = screen_width;
+
+            if (ID == 1)
+            {
+                this.name = "Enemey1";
+                this.HP = 100;
+                this.DMG = 100;
+            }
+            else if (ID==2)
+            {
+                this.name = "Enemy2";
+                this.HP = 200;
+                this.DMG = 100;
+
+            }
+            if(ID==3)
+            {
+                this.name = "Enemy3";
+                this.HP = 300;
+                this.DMG = 100;
+            }
+            if(ID==4)
+            {
+                this.name = "Boss";
+                this.HP = 20000;
+                this.DMG = 50;
+            }
+        }
+        public void shoot()
+        {
+
+        }
+
+        public void update(game game1)
+        {
+            X = X  -1;
+        }
+
+        public void draw(SpriteBatch spritebatch)
+        {
+            Rectangle destinationRectangle = new Rectangle(this.X, this.Y, (int)this.screen_width / 5, (int)this.screen_height / 5);
+
+            if     (this.ID == 1){ spritebatch.Draw(All_images[1], destinationRectangle, Color.White);}
+            else if(this.ID == 2){ spritebatch.Draw(All_images[2], destinationRectangle, Color.White);}
+            else if(this.ID == 3){ spritebatch.Draw(All_images[3], destinationRectangle, Color.White);}
+            else if(this.ID == 4){ spritebatch.Draw(All_images[4], destinationRectangle, Color.White);}
+
+
+        }
+        public List<button> buttons
+        {
+            get
+            {
+                return this.Buttons;
+            }
+        }
+        public List<textbox> textboxes
+        {
+            get
+            {
+                return Textboxes;
+            }
+        }
+    }
+}
---
+++ Project_game4/Project_game4/Level_Boss.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using project_4_algemeen;
+
+namespace project_4_algemeen
+{
+    class Level_Boss : gameElement
+    {
+        // thins that this class will receive : Player , enemies , list of images 
+        List<Enemy> Enemies = new List<Enemy>();
+        List<Texture2D> All_images = new List<Texture2D>();
+        string name;
+        double screen_width, screen_height;
+        List<button> Buttons = new List<button>();
+        List<textbox> Textboxes = new List<textbox>();
+        Player player1;
+        game game1;
+
+        public Level_Boss(string name, double screen_width, double screen_height, List<Texture2D> All_images,game game1)
+        {
+
+            this.screen_width = screen_width;
+            this.screen_height = screen_height;
+            this.All_images = All_images;
+            this.game1 = game1;
+
+            Enemies.Add(new Enemy(4, 800, 200, this.screen_width, this.screen_height, All_images));
+
+            player1 = new Player(this.All_images, this.game1,this.screen_width,this.screen_height);
+
+        }
+        public List<button> buttons
+        {
+            get
+            {
+                return this.Buttons;
+            }
+        }
+        public List<textbox> textboxes
+        {
+            get
+            {
+                return Textboxes;
+            }
+        }
+
+        public void draw(SpriteBatch spritebatch)
+        {
+            Rectangle destinationRectangle = new Rectangle(0, 0, (int)this.screen_width, (int)this.screen_height);
+            spritebatch.Draw(All_images[0], destinationRectangle, Color.White);
+            foreach (Enemy enemy in Enemies)
+            {
+                enemy.draw(spritebatch);
+            }
+            player1.draw(spritebatch);
+        }
+
+        public void update(game game1)
+        {
+            player1.update(game1);
+            foreach (Enemy enemy in Enemies)
+            {
+                enemy.update(game1);
+            }
+        }
+    }
+}
