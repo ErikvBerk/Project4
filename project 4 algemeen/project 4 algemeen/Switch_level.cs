16e3ab718b182b73bdd29eb3b050727c97517edf
ErikvBerk <erik.van.berk@gmail.com>
2017-07-04 20:55:46 +0200

    werkende replay(2)

--- project 4 algemeen/project 4 algemeen/Switch_level.cs
+++ project 4 algemeen/project 4 algemeen/Switch_level.cs
@@ -63,7 +63,7 @@ namespace project_4_algemeen
                     player1 = new AndroidPlayer(All_images, game1, screen_width, screen_height, font, graphics, platform,1000,25, sound);
                     break;
                 case platform.windows:
-                    player1 = new Player(this.All_images, this.game1, this.screen_width, this.screen_height, platform, 1000, 25, sound);
+                    player1 = new Player(this.All_images, this.game1, this.screen_width, this.screen_height, platform, 10000000 ,500, sound);
                     break;
                 default:
                     break;
@@ -80,19 +80,13 @@ namespace project_4_algemeen
             levelBoss = new Level_Boss("Level Boss", this.screen_width, this.screen_height, this.All_images, this.game1, this.platform, this.Font, this.graphics, this.player1, projectiles, sound);
 
 
+            
 
 
-            endScreen.levelcleared = false;
-            endScreen2.levelcleared = false;
-            endScreen3.levelcleared = false;
-            endScreen4.levelcleared = false;
-            endScreen5.levelcleared = false;
-            level1.levelcleared = false;
-            level2.levelcleared = false;
-            level3.levelcleared = false;
-            levelBoss.levelcleared = false;
+
 
             current = level1;
+            
 
         }
         public List<button> buttons
@@ -111,84 +105,85 @@ namespace project_4_algemeen
         }
         public void update(game game1)
         {
-            current.update(game1);
+
             Score = player1.currentscore;
-            if (current == level1 && level1.levelcleared == true || player1.dead == true) //checks if current is level 1 and if level 1 is cleared and then changes the current to the first 'endscreen'
+            current.update(game1);
+
+            if (player1.dead == true)
             {
-                if (player1.dead == true)
-                {
-                    endScreen5.UpdateScore(Score);
-                    current = endScreen5;
-                    endScreen5.GameLose();
-                    game1.resetButtons();
-                }
-                else
+                endScreen5.UpdateScore(Score);
+                current = endScreen5;
+                endScreen5.GameLose();
+                game1.resetButtons();
+            }
+
+            if (current == level1) //checks if current is level 1 and if level 1 is cleared and then changes the current to the first 'endscreen'
+            {
+                
+
+                if (level1.levelcleared == true)
                 {
                     endScreen.UpdateScore(Score);
                     current = endScreen;
                     game1.resetButtons();
                 }
+            }
 
-            }
-            else if (current == endScreen && endScreen.levelcleared == true)
+
+
+            else if (current == endScreen)
             {
-                current = level2;
-                game1.resetButtons();
-            }
-            else if (current == level2 && level2.levelcleared == true || player1.dead == true)
-            {
-                if (player1.dead == true)
+                if (endScreen.levelcleared == true)
                 {
-                    endScreen5.UpdateScore(Score);
-                    current = endScreen5;
-                    endScreen5.GameLose();
+                    current.update(game1);
+                    current = level2;
                     game1.resetButtons();
                 }
-                else
+            }
+
+            else if (current == level2)
+            {
+
+                if (level2.levelcleared == true)
                 {
                     endScreen2.UpdateScore(Score);
                     current = endScreen2;
                     game1.resetButtons();
                 }
             }
-            else if (current == endScreen2 && endScreen2.levelcleared == true)
+
+            else if (current == endScreen2)
             {
-                current = level3;
-                game1.resetButtons();
-            }
-            else if (current == level3 && level3.levelcleared == true || player1.dead == true)
-            {
-                if (player1.dead == true)
+                if (endScreen2.levelcleared == true)
                 {
-                    endScreen5.UpdateScore(Score);
-                    current = endScreen5;
-                    endScreen5.GameLose();
+                    current = level3;
                     game1.resetButtons();
                 }
-                else
+            }
+            else if (current == level3)
+            {
+                if (level3.levelcleared == true)
                 {
                     endScreen3.UpdateScore(Score);
                     current = endScreen3;
                     game1.resetButtons();
                 }
             }
-            else if (current == endScreen3 && endScreen3.levelcleared == true)
+
+            else if (current == endScreen3)
             {
-                sound.UnicornSound();
-                current = levelBoss;
-                game1.resetButtons();
-            }
-            else if (current == levelBoss && levelBoss.levelcleared == true || player1.dead == true)
-            {
-                if (player1.dead == true)
+                if (endScreen3.levelcleared == true)
                 {
-                    endScreen5.UpdateScore(Score);
-                    current = endScreen5;
-                    endScreen5.GameLose();
+                    sound.UnicornSound();
+                    current = levelBoss;
                     game1.resetButtons();
                 }
-                else
+            }
+            else if (current == levelBoss)
+            {
+                if (levelBoss.levelcleared == true)
                 {
+
                     endScreen4.UpdateScore(Score);
                     current = endScreen4;
                     endScreen4.GameWon();
@@ -196,14 +191,35 @@ namespace project_4_algemeen
                 }
             }
 
-            else if (current == endScreen4 && endScreen4.levelcleared == true)
+
+            else if (current == endScreen4)
             {
-                current = Origin;
-                game1.resetButtons();
+                if (endScreen4.levelcleared == true)
+                {
+
+                    endScreen = new EndScreen(false, Font, screen_width, screen_height, graphics, game1, platform, Score, this.sound, "Next level");
+                    endScreen2 = new EndScreen(false, Font, screen_width, screen_height, graphics, game1, platform, Score, this.sound, "Next level");
+                    endScreen3 = new EndScreen(false, Font, screen_width, screen_height, graphics, game1, platform, Score, this.sound, "Next level");
+                    endScreen4 = new EndScreen(false, Font, screen_width, screen_height, graphics, game1, platform, Score, this.sound, "Go back to menu");
+                    endScreen5 = new EndScreen(true, Font, screen_width, screen_height, graphics, game1, platform, Score, this.sound, "Go back to menu");
+                    level1 = new Level_1("Level 1", this.screen_width, this.screen_height, this.All_images, this.game1, this.platform, this.Font, this.graphics, this.player1, projectiles, sound);
+                    level2 = new Level_2("Level 2", this.screen_width, this.screen_height, this.All_images, this.game1, this.platform, this.Font, this.graphics, this.player1, projectiles, sound);
+                    level3 = new Level_3("Level 3", this.screen_width, this.screen_height, this.All_images, this.game1, this.platform, this.Font, this.graphics, this.player1, projectiles, sound);
+                    levelBoss = new Level_Boss("Level Boss", this.screen_width, this.screen_height, this.All_images, this.game1, this.platform, this.Font, this.graphics, this.player1, projectiles, sound);
+                    player1.currentscore = 0;
+
+                    game1.current = Origin;
+                    game1.resetButtons();
+                    current = level1;
+
+                }
+
+
             }
-                
-            
-            
+
+
+
+
         }
         public void draw(SpriteBatch spritebatch)
         {
